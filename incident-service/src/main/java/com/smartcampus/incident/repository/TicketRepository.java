c71950684760c969044e789ea163f400d23ab12e
Seneth Jayashan <sama.gunasena12@gmail.com>
2026-04-11 17:05:21 +0530

    Remove incident-service module
    Delete the entire incident-service module and its contents: project metadata (pom.xml), Maven wrapper (.mvn, mvnw, mvnw.cmd), .gitattributes/.gitignore, application.properties, and all Java sources (main application, controllers, services, entities, repositories). This removes the incident-service microservice from the repository.

--- incident-service/src/main/java/com/smartcampus/incident/repository/TicketRepository.java
+++
@@ -1,7 +0,0 @@
-package com.smartcampus.incident.repository;
-
-import com.smartcampus.incident.entity.Ticket;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TicketRepository extends JpaRepository<Ticket, Long> {
-}
